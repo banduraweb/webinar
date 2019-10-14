ae451e81a57c8e773bdcce58cdf8bb5dbcb4faa7
bandura <banduraweb@ukr.net>
2019-10-14 18:00:34 +0300

    Updates

---
+++ main.js
@@ -0,0 +1,9 @@
+(()=> {
+    fetch('https://api.privatbank.ua/p24api/pubinfo?exchange&json&coursid=11')
+        .then(data=>data.json())
+        .then(data=>data.map(item=>document.getElementById('rate-container').innerHTML +=
+            `<td>${item.ccy}</td> <td>${item.base_ccy}</td> <td>${item.buy} <td>${item.sale}</td>`))
+})();
+
+
+
